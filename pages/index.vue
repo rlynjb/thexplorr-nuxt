741db695f5753fe20d7bb4396d45587c15f0094c
rlyn <rlynjb@gmail.com>
2020-05-09 19:27:52 +0000

    added form style

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,6 @@
 <template>
-  <v-layout
-    column
-    justify-center
-    align-center
-  >
-    <v-flex
-      xs12
-      sm8
-      md6
-    >
+  <v-layout column justify-center align-center>
+    <v-flex xs12 sm8 md6>
 
       <v-card>
         <v-card-title class="headline">
@@ -16,7 +8,7 @@
         </v-card-title>
 
         <v-card-text>
-          <v-row>
+          <v-row no-gutters>
             <v-col cols="12">
               <v-text-field
                 label="What are you gonna call your adventure?"
@@ -27,46 +19,118 @@
 
             <v-col cols="12">
               <v-text-field
-                label="any awesome things to experience?"
-                placeholder="ex. witness history"
+                label="Where?"
+                placeholder="ex. Las Vegas, Hollywood, St. George, etc"
                 filled
               />
             </v-col>
 
-            visited on:
-            note
-            day/night experience:
-            - name
-            - price
-            - address, city, state, country
-            - note
+            <v-col cols="12">
+              <v-textarea
+                auto-grow
+                filled
+                name="input-7-4"
+                label="What's the main awesome things to experience?"
+                value=""
+              >
+              </v-textarea>
+            </v-col>
 
+            <v-col cols="6" class="pr-3">
+              <v-text-field
+                label="When?"
+                placeholder="ex. this weekend, next month, June 29, 2020 (Friday)"
+                filled
+              />
+            </v-col>
 
+            <v-col cols="6" class="pl-3">
+              <v-text-field
+                label="Until"
+                placeholder="ex. July 04, 2020 (Sunday)"
+                filled
+              />
+            </v-col>
           </v-row>
 
-          <hr class="my-3">
-          <a
-            href="https://nuxtjs.org/"
-            target="_blank"
-          >
-            Nuxt Documentation
-          </a>
-          <br>
-          <a
-            href="https://github.com/nuxt/nuxt.js"
-            target="_blank"
-          >
-            Nuxt GitHub
-          </a>
+
+          <v-expansion-panels multiple accordion focusable>
+            <v-expansion-panel expand>
+              <v-expansion-panel-header>During Day time</v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-row>
+                  <v-list>
+                    <v-list-item>
+                      <v-row no-gutters>
+                        <v-col cols="8" class="pr-3">
+                          <v-text-field
+                            label="Name"
+                            placeholder="Original Starbucks coffee shop"
+                            filled
+                          />
+                        </v-col>
+                        <v-col cols="4" class="pl-3">
+                          <v-text-field
+                            label="Price"
+                            placeholder="5.00"
+                            filled
+                          />
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field
+                            label="Where?"
+                            placeholder="8555 N. 4th st. Las Vegas, NV 89101, US"
+                            filled
+                          />
+                        </v-col>
+                        <v-col cols="12">
+                          <v-textarea
+                            auto-grow
+                            filled
+                            name="input-7-4"
+                            label="Any other info?"
+                            value=""
+                          >
+                          </v-textarea>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-btn color="primary" nuxt to="/inspire">+</v-btn>
+                        </v-col>
+                      </v-row>
+
+
+                    </v-list-item>
+                  </v-list>
+                </v-row>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+
+            <v-expansion-panel>
+              <v-expansion-panel-header>During Night time</v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-row>
+                  <ul>
+                    <li>
+                      - name
+                      - price<br>
+                      - address, city, state, country<br>
+                      - note
+
+                      <v-spacer />
+                      <v-btn color="primary" nuxt to="/inspire">+</v-btn>
+                    </li>
+                  </ul>
+                </v-row>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+
         </v-card-text>
+
+
         <v-card-actions>
-          <v-spacer />
-          <v-btn
-            color="primary"
-            nuxt
-            to="/inspire"
-          >
-            Continue
+          <v-btn color="primary" nuxt to="/inspire">
+            Log It / Share
           </v-btn>
         </v-card-actions>
       </v-card>
